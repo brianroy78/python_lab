875ddd73096ac6f57a45e92c413d8f8e12a4b46a
Brian Roy Sagredo Lijeron <brianroy78@hotmail.com>
2023-01-21 15:20:59 -0400

    some progress

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -3,9 +3,12 @@ import itertools as it
 import operator as op
 
 import functions as f
+import functor as fu
 
 is_valid = ft.partial(op.le, 0)
-count_parentheses = ft.partial(f.switch_case, 0, (f.Premise('(', 1), f.Premise(')', -1)))
+count_parentheses = ft.partial(
+    f.switch_case, 0, (f.Premise("(", 1), f.Premise(")", -1))
+)
 
 
 def main_logic(length, counts):
@@ -14,14 +17,17 @@ def main_logic(length, counts):
     return are_valid(others) and next(counts) == 0
 
 
-def valid_parentheses(string):
+def valid_parentheses(string: str) -> bool:
     if f.is_empty(string):
         return True
-    return f.compose(
-        f.mapper(count_parentheses),
-        ft.partial(it.accumulate, func=op.add),
-        ft.partial(main_logic, len(string))
-    )(string)
+    a: list[str] = [s for s in string]
+    return (
+        fu.IterFunctor  (a)
+        .map(count_parentheses)
+        .apply(ft.partial(it.accumulate, func=op.add))
+        .flat(ft.partial(main_logic, len(string)))
+        .value
+    )
 
 
 print(valid_parentheses("  ("), False, "should work for '  ('")
@@ -29,4 +35,3 @@ print(valid_parentheses(")test"), False, "should work for ')test'")
 print(valid_parentheses(""), True, "should work for ''")
 print(valid_parentheses("hi())("), False, "should work for 'hi())('")
 print(valid_parentheses("hi(hi)()"), True, "should work for 'hi(hi)()'")
-
